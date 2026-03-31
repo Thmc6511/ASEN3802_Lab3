34192a26642dd93809ba2257cdcc15dda5fc87d5
samwieder1 <sawi7871@colorado.edu>
2026-03-31 12:06:43 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,16 @@
-clc
-clear
-close all
+clc;
+clear;
+close all;
+
+%% Read in NACA files
+
+NACA = 2412;
+digit_array = num2str(NACA);
+
+for i = 1:4
+    digit(i) = str2num(digit_array(i));
+end
+
+m = digit(1);
+p = digit(2);
+t = digit(3) * 10 + digit(4);
