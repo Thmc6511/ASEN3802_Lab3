556392a4f9d1f797e61c5dd238f8a8fab74d0df6
samwieder1 <sawi7871@colorado.edu>
2026-03-31 12:09:06 -0600

    Update main.m

--- main.m
+++ main.m
@@ -4,13 +4,20 @@ close all;
 
 %% Read in NACA files
 
+% Defining NACA airfoil and turning it into a character
 NACA = 2412;
 digit_array = num2str(NACA);
 
+% Turning Matrix into an array of each digit
 for i = 1:4
     digit(i) = str2num(digit_array(i));
 end
 
+% Defining the airfoil parameters
 m = digit(1);
 p = digit(2);
 t = digit(3) * 10 + digit(4);
+
+% Airfoil function
+[x_b,y_b] = NACA_Airfoils(m,p,t,c,N);
+
