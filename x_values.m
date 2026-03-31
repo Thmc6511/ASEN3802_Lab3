afd27ecc8e1a30d4b485e3144d5f0067425f0be1
Carson Schlageter <carson.schlageter@colorado.edu>
2026-03-31 12:54:03 -0600

    update

--- x_values.m
+++ x_values.m
@@ -1,4 +1,4 @@
-function [x_values] = x_values(chord_length, number_of_values)
+function [x_values] = x_values(chord_length, number_of_panels_per_surface)
 %Summary
 % This function uses equiangular rays fro the center fo the airfoil to a
 % circle which fits the entire airfoil. The purpose of this function is to
@@ -8,6 +8,7 @@ function [x_values] = x_values(chord_length, number_of_values)
 % Author: Carson Schlageter
 % Collaborators: 
 % Date: 3/31/2026
+number_of_values = number_of_panels_per_surface * 2;
 theta = linspace(0,360, number_of_values);
 
 r = chord_length/2;
